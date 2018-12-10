09eb4cc5d18dfec8d6b69cfd54b776ebe5a31391
jcsoutherland <jcsouthyy@gmail.com>
2018-12-10 10:40:22 -0600

    added only useful code

---
+++ gamedata.h
@@ -0,0 +1,10 @@
+#ifndef gamedata_h
+#define gamedata_h
+
+unsigned char map[];
+unsigned char map2[];
+unsigned char spriteTiles[];
+unsigned char stickguy[];
+unsigned char tile[];
+unsigned char scoreboard[];
+unsigned char cloud[];
---
+++ player.h
@@ -0,0 +1,9 @@
+#ifndef Player_h
+#define Player_h
+
+typedef struct Player{
+	int x;
+	int y;
+}Player;
+
+#endif 
